b761f95035ccd3663c422a20b61c33bed5875ece
ataMayel <ataboter@gmail.com>
2023-10-31 16:22:06 +0330

    no changes

--- combination.js
+++ combination.js
@@ -1,5 +1,4 @@
 function Tarkib(n, r) {
     if (r == 0 || r == n) return 1
-    else return Tarkib(n - 1, r) + Tarkib(n - 1, r - 1);
+    else return Tarkib(n - 1, r) + Tarkib(n - 1, r - 1)
 }
-console.log(Tarkib(8,3));
